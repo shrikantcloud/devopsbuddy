5b37424ed693bcfd1cc8adfc9c720d90ee9ca5a8
Shrikant Agiwal <shrikant.cloud@gmail.com>
2017-12-25 14:16:45 +0530

    added bootstrap

--- src/main/java/com/devopsbuddy/controllers/HelloWorldController.java
+++ src/main/java/com/devopsbuddy/controllers/HelloWorldController.java
@@ -1,14 +1,11 @@
 package com.devopsbuddy.controllers;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-@Controller
+//@Controller
 public class HelloWorldController {
-	
-	@RequestMapping("/")
-	public String sayHello() {
-		return "index";
-	}
+
+    // @RequestMapping("/")
+    public String sayHello() {
+        return "index";
+    }
 
 }
