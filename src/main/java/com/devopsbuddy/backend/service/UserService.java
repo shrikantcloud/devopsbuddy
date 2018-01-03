b19c023cd41a0fec57bb35761778792cfa6269f7
Shrikant Agiwal <shrikant.cloud@gmail.com>
2018-01-03 21:54:00 +0530

    integrated with mysql db

--- src/main/java/com/devopsbuddy/backend/service/UserService.java
+++ src/main/java/com/devopsbuddy/backend/service/UserService.java
@@ -9,9 +9,11 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.devopsbuddy.backend.persistence.domain.backend.PasswordResetToken;
 import com.devopsbuddy.backend.persistence.domain.backend.Plan;
 import com.devopsbuddy.backend.persistence.domain.backend.User;
 import com.devopsbuddy.backend.persistence.domain.backend.UserRole;
+import com.devopsbuddy.backend.persistence.repositories.PasswordResetTokenRepository;
 import com.devopsbuddy.backend.persistence.repositories.PlanRepository;
 import com.devopsbuddy.backend.persistence.repositories.RoleRepository;
 import com.devopsbuddy.backend.persistence.repositories.UserRepository;
@@ -30,25 +32,34 @@ public class UserService {
     private UserRepository        userRepository;
     @Autowired
     private BCryptPasswordEncoder passwordEncoder;
+    @Autowired
+    private PasswordResetTokenRepository passwordResetTokenRepository;
+    
 
     @Transactional
     public User createUser(User user, PlansEnum plansEnum, Set<UserRole> userRoles) {
+        
+        User localUser = userRepository.findByEmail(user.getEmail());
+        
+        if(localUser!=null) {
+            LOG.info("User with username {} and email {} already exists. Nothing will be done", user.getUsername(), user.getEmail());
+        } else {
+            String encryptedPassword = passwordEncoder.encode(user.getPassword());
+            user.setPassword(encryptedPassword);
 
-        String encryptedPassword = passwordEncoder.encode(user.getPassword());
-        user.setPassword(encryptedPassword);
+            Plan plan = new Plan(plansEnum);
+            if (!planRepository.exists(plansEnum.getId())) {
+                planRepository.save(plan);
+            }
 
-        Plan plan = new Plan(plansEnum);
-        if (!planRepository.exists(plansEnum.getId())) {
-            planRepository.save(plan);
+            user.setPlan(plan);
+            for (UserRole ur : userRoles) {
+                roleRepository.save(ur.getRole());
+            }
+            user.getUserRoles().addAll(userRoles);
+            localUser = userRepository.save(user);
         }
-
-        user.setPlan(plan);
-        for (UserRole ur : userRoles) {
-            roleRepository.save(ur.getRole());
-        }
-        user.getUserRoles().addAll(userRoles);
-        userRepository.save(user);
-        return user;
+        return localUser;
     }
 
     @Transactional
@@ -56,6 +67,11 @@ public class UserService {
         String encodedPassword = passwordEncoder.encode(password);
         userRepository.updateUserPassword(userId, encodedPassword);
         LOG.debug("Password updated successfully for userID {}", userId);
+        
+        Set<PasswordResetToken> resetTokens = passwordResetTokenRepository.findAllByUserId(userId);
+        if(!resetTokens.isEmpty()) {
+            passwordResetTokenRepository.delete(resetTokens);
+        }
     }
 
     public User findByUserName(String username) {
